e96ab4330ea96b20c5bea089ccd6d7353909fde0
constantine <okonemmanuelmma@gmail.com>
2025-01-02 20:30:50 +0100

    initial commit

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("26KetVkE2tjxfPGtxWnfTYLWG2WZDCPfZ8w2KuiSqdfL");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
