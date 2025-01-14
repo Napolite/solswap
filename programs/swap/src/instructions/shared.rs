531d9b3192d549588f4d95ec83c9cd97c3b738ae
constantine <okonemmanuelmma@gmail.com>
2025-01-14 11:27:32 +0100

    init

---
+++ programs/swap/src/instructions/shared.rs
@@ -0,0 +1,7 @@
+use anchor_spl::token_interface::TokenAccount;
+
+use anchor_lang::prelude::*;
+
+pub fn transfer_tokens<'info>(from: &InterfaceAccount<'info, TokenAccount>) -> Result<()> {
+    return Ok(());
+}
