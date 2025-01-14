9873a45afd7b18887c6ec98a3035f793bdd4b0c6
constantine <okonemmanuelmma@gmail.com>
2025-01-14 11:59:35 +0100

    init

--- programs/swap/src/instructions/shared.rs
+++ programs/swap/src/instructions/shared.rs
@@ -1,7 +1,25 @@
-use anchor_spl::token_interface::TokenAccount;
+use anchor_spl::{
+    token::TransferChecked,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
 use anchor_lang::prelude::*;
 
-pub fn transfer_tokens<'info>(from: &InterfaceAccount<'info, TokenAccount>) -> Result<()> {
+pub fn transfer_tokens<'info>(
+    from: &InterfaceAccount<'info, TokenAccount>,
+    to: &InterfaceAccount<'info, TokenAccount>,
+    amount: &u64,
+    mint: &InterfaceAccount<'info, Mint>,
+    authority: &Signer<'info>,
+    token_program: &Interface<'info, TokenInterface>,
+) -> Result<()> {
+    let transfer_accounts_options = TransferChecked {
+        from: from.to_account_info(),
+        to: to.to_account_info(),
+        authority: authority.to_account_info(),
+        mint: mint.to_account_info(),
+    };
+
+    let cpi_context = CpiContext::new(token_program.to_account_info(),transfer_accounts_options)
     return Ok(());
 }
